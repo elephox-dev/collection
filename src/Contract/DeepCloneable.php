9c9d4601f8e8027ef391473897f3815c3c03aa7e
Ricardo Boss <contact@ricardoboss.de>
2021-12-16 16:58:48 +0100

    Moved DeepCloneable closer to trait

--- src/Contract/DeepCloneable.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Elephox\Collection\Contract;
-
-interface DeepCloneable
-{
-	public function deepClone(): self;
-}
--- src/Contract/GenericCollection.php
+++ src/Contract/GenericCollection.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Elephox\Collection\Contract;
 
+use Elephox\Support\Contract\DeepCloneable;
+
 /**
  * @template T
  *
