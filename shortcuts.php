f0ad4b113881ce618f0d515f8ca7b4329670e05e
Ricardo Boss <contact@ricardoboss.de>
2022-11-01 23:10:48 +0100

    Fix CS

--- shortcuts.php
+++ shortcuts.php
@@ -5,7 +5,8 @@ use Elephox\Collection\Contract\GenericEnumerable;
 use Elephox\Collection\Enumerable;
 
 if (!function_exists('collect')) {
-	function collect(mixed... $values): GenericEnumerable {
+	function collect(mixed ...$values): GenericEnumerable
+	{
 		return Enumerable::from($values);
 	}
 }
